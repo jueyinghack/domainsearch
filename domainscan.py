1fd7e3e31be3d967111384f522547b4d7be11b5c
jueyinghack <62453288+jueyinghack@users.noreply.github.com>
2021-08-16 16:07:06 +0800

    Update domainscan.py

--- domainscan.py
+++ domainscan.py
@@ -1,8 +1,7 @@
 import requests
 import re
 import sys
-from openpyxl import Workbook, load_workbook
-from lxml import etree
+from openpyxl import Workbook
 # 对表格的操作
 wb = Workbook()
 ws1 = wb.create_sheet("Mysheet")           #创建一个sheet
@@ -10,12 +9,13 @@ line = 2
 ws1["A1"] = "domain"
 ws1["B1"] = "title"
 ws1["C1"] = "ip"
-arg1 = int(sys.argv[1])
+arg1 = sys.argv[1]
 arg2 = int(sys.argv[2])
+arg3 = int(sys.argv[3])
 
-for ip1 in range(arg1, arg2):
+for ip1 in range(arg2, arg3):
     for ip2 in range(1,256):
-        url = "http://gcsec.top:81/Less-1/?name=210.44.{}.{}".format(ip1,ip2)
+        url = "http://gcsec.top:81/Less-1/?name={}.{}.{}".format(arg1,ip1,ip2)
         print(url)
         try:
             text = requests.get(url,timeout=3).content.decode()
@@ -33,12 +33,12 @@ for ip1 in range(arg1, arg2):
                 except:
                     ws1["A" + str(line)] = "http://"+i
                     ws1["B" + str(line)] = "no"
-                    ws1["C" + str(line)] = "210.44.{}.{}".format(ip1,ip2)
+                    ws1["C" + str(line)] = "{}.{}.{}".format(arg1,ip1,ip2)
                     line += 1
                 else:
                     ws1["A" + str(line)] = "http://" + i
                     ws1["B" + str(line)] = str(title)
-                    ws1["C" + str(line)] = "210.44.{}.{}".format(ip1,ip2)
+                    ws1["C" + str(line)] = "{}.{}.{}".format(arg1,ip1,ip2)
                     line += 1
 
     wb.save("{}.xlsx".format(ip1))
